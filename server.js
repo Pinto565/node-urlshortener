46ebf19d67cdf9b276067b4608322c956b4d1e47
Pinto565 <intoinfant5650@gmail.com>
2021-10-09 10:20:33 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const shortUrl = require("./models/shortUrl");
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://pinto:zxcvbnm%40123@cluster0.tb80h.mongodb.net/urlShortener",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  const shortUrls = await shortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.post("/shortUrls", (req, res) => {
+ shortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrls = await shortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrls == null) return res.sendStatus(404)
+
+  shortUrls.clicks++
+  shortUrls.save()
+  res.redirect(shortUrls.full)
+});
+
+app.listen(process.env.PORT || 5000);
